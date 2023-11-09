208024c0348039b8a41418a45b51f9ea2e45396e
code406 <davidpm99@gmail.com>
2023-11-09 14:57:21 +0100

    feat: they changed the site. new element names & logic

--- dynadot.py
+++ dynadot.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import logging
+from time import sleep
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -32,53 +33,71 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # Navigate to the Dynadot search page
 logging.info("Navigating to the Dynadot search page...")
-driver.get("https://www.dynadot.com/domain/search.html")
+driver.get("https://www.dynadot.com/domain/search")
 
 # Find the domain search input element and enter your domain name
-driver.find_element(By.ID, "domain_search_input").send_keys(args.name)
+logging.info("Looking for the domain search input element...")
+driver.find_element(By.ID, "search-domain-input").send_keys(args.name)
 
 # Find and click the search button
-logging.info('Clicking the "search-submit" button...')
-driver.find_element(By.CSS_SELECTOR, ".btn.search-submit").click()
+logging.info('Clicking the search button...')
+driver.find_element(By.ID, "search-button").click()
 
 # Find and click the "accept_button" button (to dismiss the privacy policy popup)
 logging.info('Clicking the "accept-privacy" button...')
 try:
-    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "accept_button"))).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "accept_button"))).click()
 except TimeoutException:
     logging.info('"accept-privacy" button not found. Continuing...')
 
 # Wait for the "view-more-button" element to be visible. While it's visible, click it
 logging.info('Clicking the "view-more-button" button until all domains are loaded...')
-while True:
+attempts_left = 2
+while attempts_left > 0:
     try:
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "view-more-button"))).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "see-more-group"))).click()
     except (TimeoutException, NoSuchElementException, StaleElementReferenceException):
-        break
+        logging.info("That's it? Will retry in 5 seconds, just in case...")
+        sleep(5)
+        attempts_left -= 1
+
+        # Check if search quota exceeded (class "domain-search-result-error" is visible)
+        if driver.find_element(By.CLASS_NAME, "domain-search-result-error").is_displayed():
+            logging.info("Search quota exceeded. Maybe try again later? Exiting...")
+            driver.quit()
+            exit(1)
 
 # Save contents and close the WebDriver
 page_contents = driver.page_source
 driver.quit()
 
-# Parse the HTML. We want to find the "tab-result" div
+# Parse the HTML. We want to find the first div with class="domain-search-result"
 logging.info("Done with Selenium! Parsing the HTML with BeautifulSoup...")
 soup = BeautifulSoup(page_contents, "html.parser")
-tab_result = soup.find(id="tab-result")
-assert isinstance(tab_result, Tag)
+search_result = soup.find("div", {"class": "domain-search-result"})
+assert isinstance(search_result, Tag)
 
 # Build the list of available domains
-domains = []
+domains = {}
 currency = None
 
-# Each domain is in a div.class="s-row transition-all"
-for div in tab_result.find_all("div", {"class": "s-row transition-all"}):
-    # If the div contains the "fa-cart-plus" icon, it's available
-    if div.find("i", {"class": "fa-cart-plus"}):
-        # Get domain name, TLD, price and renewal price from spans in the div
-        name = div.find("span", {"class": "result-idn-name"}).text
-        tld = div.find("span", {"class": "result-idn-tld"}).text
-        price = div.find("span", {"class": "s-current-price"}).text
-        renewal = div.find("span", {"class": "s-renewal-price"}).text
+# Each domain is in a div.class="search-row"
+for div in search_result.find_all("div", {"class": "search-row"}):
+
+    # If the div contains the "search-shop-cart" icon, it's available
+    if div.find("div", {"class": "search-shop-cart"}):
+        
+        # Get TLD from the "row-tld" attribute of the search-row div
+        tld = div["row-tld"]
+
+        # Get name, price, renewal from classes search-domain, search-price & search-renewal
+        name = div.find("div", {"class": "search-domain"}).text
+        price = div.find("div", {"class": "search-price"}).text
+
+        # Divs come twice, 1 with and 1 without renewal. If there's no renewal, skip
+        try: renewal = div.find("div", {"class": "search-renewal"}).text
+        except AttributeError: continue
+
         if not currency:
             currency = price[0]
 
@@ -95,9 +114,10 @@ for div in tab_result.find_all("div", {"class": "s-row transition-all"}):
             and (args.non_ascii or tld.isascii())
             and (args.slds or "." not in tld)
         ):
-            domains.append({"name": f"{name}.{tld}", "price": price, "renewal": renewal})
+            domains[name] = {"name": f"{name}", "price": price, "renewal": renewal}
 
 # Sort by domain length, then by renewal, then by price
+domains = list(domains.values())
 domains.sort(key=lambda x: (len(x["name"]), x["renewal"], x["price"]))
 
 # If output_file given, write the available domains to it. Otherwise, print to console
